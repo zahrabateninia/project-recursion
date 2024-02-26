12b618de34c75eab4c93e1094d2b15daa35c6800
zahrabateninia <zahrabatenin@gmail.com>
2024-02-26 21:48:34 +0330

    Generate and return an array containing the Fibonacci sequence up to the given number

--- Fibonacci/fibRec.js
+++ Fibonacci/fibRec.js
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 
-function fibonacci(n){
-    if(n<2){
-        return n
-    }else{
-        return fibonacci(n-1) + fibonacci(n-2)
+function fibonacci(n, sequence = []) {
+    if (n === 0) {
+        return sequence;
     }
+    if (sequence.length < 2) {
+        sequence.push(sequence.length);
+        return fibonacci(n, sequence);
+    }
+    const nextFibonacci = sequence[sequence.length - 1] + sequence[sequence.length - 2];
+    if (nextFibonacci <= n) {
+        sequence.push(nextFibonacci);
+        return fibonacci(n, sequence);
+    }
+    return sequence;
 }
 
-let result = fibonacci(5)
-console.log(result)
+let result = fibonacci(5);
+console.log(result);
