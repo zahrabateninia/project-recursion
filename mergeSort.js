7aad2b02501c8eccdc58c3f5b6053e1d0e109458
zahrabateninia <zahrabatenin@gmail.com>
2024-03-06 14:24:56 +0330

    Fix the error

--- mergeSort.js
+++ mergeSort.js
@@ -2,6 +2,57 @@
 
 // From the pseudocode of Introduction To Algorithms book page 46
 
+
+function merge(A, p, q, r){
+    // length of A[p:q]
+    let nL = q - p + 1
+    //  length of A[q+1:r]
+    let nR = r - q
+    // Let leftArr[0 : nL-1] and rightArr[0 : nR-1] be new arrays
+    let leftArr = new Array(nL)
+    let rightArr = new Array(nR)
+    // Copy A[p:q] into leftArr[0: nL-1]
+    for(let i = 0; i < nL; i++){
+        leftArr[i] = A[p + i] 
+    }    
+    //  Copy A[q+1:r] into rightArr[0:nR-1]
+    for(let j = 0; j < nR; j++){
+        rightArr[j] = A[q + j + 1]
+    }
+    // i indexes the smallest remaining element in leftArr
+    let i = 0;
+    // j indexes the smallest remaining element in rightArr
+    let j = 0;
+    // k indexes the location in A to fill
+    let k = p;
+    // As long as each of the arrays leftArr and rightArr contains an unmerged element,
+    // copy the smallest unmerged element back into A[p:r].
+    while(i < nL && j < nR){
+        if(leftArr[i] <= rightArr[j]){
+            A[k] = leftArr[i]
+            i += 1
+        } else {
+            A[k] = rightArr[j]
+            j += 1;
+        }
+        k += 1;
+    }
+    // Having gone through one of leftArr and rightArr entirely, copy the 
+    //  remainder of the other to the end of A[p:r]
+    while(i < nL){
+        A[k] = leftArr[i]
+        i += 1
+        k += 1
+    }
+    while(j < nR){
+        A[k] = rightArr[j]
+        j += 1
+        k += 1
+    }
+
+    
+}
+
 function mergeSort(A, p, r){ // A[p:r]
     // Zero or one element
     if(p >= r){
@@ -9,64 +60,17 @@ function mergeSort(A, p, r){ // A[p:r]
     }
 
     // Midpoint of A
-    let q = Math.floor(p+r/2);
+    let q = Math.floor((p + r) / 2);
     // Recursively sort A[p:q]
     mergeSort(A, p, q)
     //  Recursively sort A[q+1:r]
-    mergeSort(A, q+1, r)
+    mergeSort(A, q + 1, r)
     // Merge A[p:q] and A[q+1:r]
     merge(A,p,q,r)
 }
 
-function merge(A, p, q, r){
-    // length of A[p:q]
-    let nL = q-p+1
-    //  length of A[q+1:r]
-    let nR = r-q
-    // Let leftArr[0 : nL-1] and rightArr[0 : nR-1] be new arrays
-    // Copy A[p:q] into leftArr[0: nL-1]
-    for(let i=0; i<= nL-1; i++){
-        leftArr[i] = A[p+i] 
-    }    
-    //  Copy A[q+1:r] into rightArr[0:nR-1]
-    for(let j=0; j<= nR-1; j++){
-        rightArr[j] = A[q+j+1]
-    }
-    // i indexes the smallest remaining element in arrLeft
-    let i = 0;
-    // j indexes the smallest remaining element in arrRight
-    let j = 0;
-    // k indexes the location in A to fill
-    let k = p;
-    // As long as each of the arrays arrLeft and arrRight contains an unmerged element,
-    // copy thn smallest unmerged element back into A[p:r].
-    while(i<nL && j<nR){
-        if(arrLeft[i]<= arrRight[j]){
-            A[k]=arrayLeft[i]
-            i +=1
-        }else{
-            A[k]=arrRight[j]
-            j+=1;
-        }
-        k+=1;
-    }
-    // Having gone through one of arrLeft and arrRight entirely, copy the 
-    //  remainder of the other to the end of A[p:r]
-    while(i<nL){
-        A[k]=arrLeft[i]
-        i =+1
-        k+=1
-    }
-    while(j<nR){
-        A[k]= arrRight[j]
-        j+=1
-        k+=1
-    }
-
-    
-}
-
 // Test
 
-let result = mergeSort([1,4,6,2,5,9,0],0, 6)
-console.log(result)
+let arr = [1, 99, 6, 2, 25, 9, 0];
+mergeSort(arr, 0, arr.length - 1);
+console.log(arr);
